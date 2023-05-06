4e10a09f0ea238277095cbcb49b800c5784cc0d5
katmai <katmai@spinningboxvr.com>
2023-05-06 14:08:14 +0200

    back on track

--- u.start.sh
+++ u.start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 #/home/katmai/.docker/cli-plugins/docker-compose stop
-#yes | /home/katmai/.docker/cli-plugins/docker-compose run -T --rm --build auto-gpt --gpt3only --continuous --ai-settings atlas.yaml
-/home/katmai/.docker/cli-plugins/docker-compose run -T --rm --build auto-gpt 
+yes | /home/katmai/.docker/cli-plugins/docker-compose run -T --rm --build auto-gpt --gpt3only --continuous --ai-settings atlas.yaml
+#/home/katmai/.docker/cli-plugins/docker-compose run -T --rm --build auto-gpt --gpt3only --ai-settings atlas.yaml
